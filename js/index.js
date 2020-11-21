5f3578c711f7ce638c4e4c229a86009bd21e8410
Tlek Suyerbassov <tleksuerbasov@mail.ru>
2020-11-21 11:33:30 +0600

    add some features

---
+++ js/index.js
@@ -0,0 +1,82 @@
+function drop(){
+	if(document.getElementById('dropdown').style.display == 'block'){
+		document.getElementById('dropdown').style.display = 'none';
+	}
+	else{
+		document.getElementById('dropdown').style.display = 'block';
+	}
+}
+
+function op(){
+	document.getElementById('menu').style.display = 'block';
+}
+
+function cl(){
+	document.getElementById('menu').style.display = 'none';
+}
+
+	new TypeIt("#typing-effect", {
+  speed: 50
+})
+  .type("Создаю и ")
+  .pause(1000)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .pause(3000)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .pause(3000)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .pause(3000)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .pause(3000)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .pause(3000)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .pause(3000)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .type("продвигаю сайты; Коучинг; Соц. сети!")
+  .pause(3000)
+  .delete(36)
+  .go();
+
+
